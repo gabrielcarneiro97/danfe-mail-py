3cde5092f9e9d681360836b573d4f7841fa85981
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-03-13 14:45:58 -0300

    Adicionei uma variável para o diretório

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 from boxReader import BoxReader
 
 
-EMAIL = os.environ['email']
-PASS = os.environ['passEmail']
+EMAIL = os.environ.get('email')
+PASS = os.environ.get('passEmail')
+
+ROOT = os.environ.get('fullDir') if os.environ.get('fullDir') is not None else './notas'
 
 
 if __name__ == "__main__":
     
-    box = BoxReader(EMAIL, PASS, root='./notas').setbox('Notas').savelasts(rm="NFe - [0-9]{8} - ", until=5140).close()
+    box = BoxReader(EMAIL, PASS, root=ROOT).setbox('Notas').savelasts(rm="NFe - [0-9]{8} - ", until=5140).close()
 
 
 
