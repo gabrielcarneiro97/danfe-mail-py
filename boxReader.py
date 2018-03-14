122eb07c857dc442eccb1d7473cde6b54c5e78f4
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-03-14 08:11:30 -0300

    Dei uma ajeitada no código

--- boxReader.py
+++ boxReader.py
@@ -4,16 +4,13 @@ import os
 import re
 import pathlib
 
-
 class BoxReader:
     imap = None
     root = None
 
     def __init__(self, email, password, mail_server='imap.gmail.com', root='./tmp'):
-
         self.root = root
         self.imap = imaplib.IMAP4_SSL(mail_server)
-
         pathlib.Path(root).mkdir(parents=True, exist_ok=True)
 
         try:
@@ -35,51 +32,37 @@ class BoxReader:
     # "NFe - [0-9]{8} - "
     def saveallatts(self, rm=''):
         rv, data = self.imap.search(None, "ALL")
-
         regex = re.compile(rm)
 
         for num in reversed(data[0].split()):
             rv, data = self.imap.fetch(num, '(RFC822)')
-
             msg = email.message_from_bytes(data[0][1])
-
             self.saveatt(msg, regex)
 
         return self
 
     def saveuntil(self, rm='', until=1):
         rv, data = self.imap.search(None, "ALL")
-
         regex = re.compile(rm)
 
         for num in reversed(data[0].split()):
-
             rv, data = self.imap.fetch(num, '(RFC822)')
-
             msg = email.message_from_bytes(data[0][1])
-
             self.saveatt(msg, regex)
-
+            
             if int(num) == until:
                 break
 
         return self
 
     def savelasts(self, rm, nm=10):
-
         rv, data = self.imap.search(None, "ALL")
-        
         regex = re.compile(rm)
-
-        
         until = int(next(reversed(data[0].split()))) - nm
 
         for num in reversed(data[0].split()):
-
             rv, data = self.imap.fetch(num, '(RFC822)')
-
             msg = email.message_from_bytes(data[0][1])
-
             self.saveatt(msg, regex)
 
             if int(num) == until:
@@ -91,24 +74,21 @@ class BoxReader:
     
 
     def saveatt(self, msg, regex):
-        subj = str(email.header.make_header(
-            email.header.decode_header(msg['Subject'])))
-
+        subj = str(email.header.make_header(email.header.decode_header(msg['Subject'])))
         folder_name = regex.sub("", subj)
 
         for part in msg.walk():
 
             if part.get_content_maintype() == 'multipart':
                 continue
+
             if part.get('Content-Disposition') is None:
                 continue
 
             filename = part.get_filename()
             folder = os.path.join(self.root, folder_name)
             att_path = os.path.join(folder, filename)
-
             pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
-
             file = part.get_payload(decode=True)
 
             if filename.endswith('.xml'):
@@ -121,5 +101,4 @@ class BoxReader:
 
     def close(self):
         self.imap.logout()
-
         return self
