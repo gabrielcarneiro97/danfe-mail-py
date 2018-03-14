503497f52e5f86799c15c6f174b95feb48fea3a2
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-03-14 08:12:12 -0300

    Mais uma arrumadinha

--- main.py
+++ main.py
@@ -2,26 +2,15 @@ import os
 import time
 from boxReader import BoxReader
 
-
 EMAIL = os.environ.get('email')
 PASS = os.environ.get('passEmail')
-
 ROOT = os.environ.get('fullDir') if os.environ.get('fullDir') is not None else './notas'
-
 REGEX = "NFe - [0-9]{8} - "
 
-
 if __name__ == "__main__":
-    
     box = BoxReader(EMAIL, PASS, root=ROOT).setbox('Notas')
 
     while True:
         print("while")
         box.savelasts(REGEX)
         time.sleep(10)
-
-
-    
-
-
-
