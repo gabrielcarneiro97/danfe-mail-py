a87c5e5126ee4fa1573b1e30d6de867636141de8
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-03-14 08:13:51 -0300

    Agora está pronto para um teste em produção

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from boxReader import BoxReader
 EMAIL = os.environ.get('email')
 PASS = os.environ.get('passEmail')
 ROOT = os.environ.get('fullDir') if os.environ.get('fullDir') is not None else './notas'
+BOX = 'Notas'
 REGEX = "NFe - [0-9]{8} - "
+SLEEP = 10
 
 if __name__ == "__main__":
-    box = BoxReader(EMAIL, PASS, root=ROOT).setbox('Notas')
+    box = BoxReader(EMAIL, PASS, root=ROOT).setbox(BOX)
 
     while True:
-        print("while")
         box.savelasts(REGEX)
-        time.sleep(10)
+        time.sleep(SLEEP)
